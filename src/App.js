e846a87a1b3f59f3040b8161cd4df99a1e60bb63
Roman Maru <romanmaru10@gmail.com>
2019-10-11 09:51:21 -0700

    Add webpack coonfiguration and routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, {Component } from 'react'
-import {Link} from 'react-router-dom'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import Users from './containers/Users'
 import asyncComponent from './hoc/asyncComponent'
 
@@ -8,16 +8,20 @@ const AsyncPizza = asyncComponent(() => {
 })
 
 class App extends Component {
-    render () {
+    render() {
         return (
             <div>
-                <Link to='/'>Users</Link>
-                <Link to='/pizza'>Pizza</Link>
-            </div>
-            <div>
-                <Route path='/' exact component={Users} />
-                <Route path='/pizza' component={AsyncPizza} />
+                <div>
+                    <Link to='/'>Users</Link>
+                    <Link to='/pizza'>Pizza</Link>
+                </div>
+                <div>
+                    <Route path='/' exact component={Users} />
+                    <Route path='/pizza' component={AsyncPizza} />
+                </div>
             </div>
         )
     }
 }
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowseRouter} from 'react-router-dom'\
+import {BrowseRouter} from 'react-router-dom'
 import App from './App'
 import './index.css'
 
